87766f36dc5de94445700ca02466e307d2c7ed01
AlexTies <alexties99@gmail.com>
2019-08-10 13:36:37 +0200

    Delete konfiguration.php

--- konfiguration.php
+++
@@ -1,9 +0,0 @@
-<?php
-error_reporting(E_ALL);
-
-define ( 'MYSQL_HOST', 'localhost' );
-define ( 'MYSQL_BENUTZER', 'hackathon_api' );
-define ( 'MYSQL_KENNWORT',  '123456789' );
-define ( 'MYSQL_DATENBANK', 'hackathon_hotelalpha' );
-define ('LANG', 2);
-?>
